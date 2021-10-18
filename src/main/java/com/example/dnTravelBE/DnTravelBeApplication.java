404db470a287f86a917f42abe54d4a9ab036c93e
văn hoàng đoàn <you@example.com>
2021-10-18 23:58:55 +0700

    create project

---
+++ src/main/java/com/example/dnTravelBE/DnTravelBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dnTravelBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnTravelBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnTravelBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dnTravelBE/DnTravelBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dnTravelBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnTravelBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
